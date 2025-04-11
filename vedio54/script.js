4d90a69928276315ed25280ffffbcd57101a4691
prafulbajpai <166216635+prafulbajpai@users.noreply.github.com>
2025-04-11 11:22:58 +0530

    Add files via upload

---
+++ vedio54/script.js
@@ -0,0 +1,12 @@
+alert("hello praful");
+console.log("code is running ")
+var a = prompt("enter a number")
+console.log("Your number is " + a)
+var istrue =confirm("are you sure you want to blast your pc")
+if(iftrue){
+    console.log("computer blasting")
+}
+else{
+    console.log("not blasting your pc ")
+}
+document.title ("blaster")
